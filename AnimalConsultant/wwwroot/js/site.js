d2d2339bcd9a08b9eaebf243c8f3cf6faab53eb9
Iryna Vasyliv <iryna.vasyliv@empeek.io>
2019-05-11 21:55:33 +0300

    thumb up and down

--- AnimalConsultant/wwwroot/js/site.js
+++ AnimalConsultant/wwwroot/js/site.js
@@ -1,4 +1,44 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿$(".like").on("click", function() {
+    event.preventDefault();
+    var $this = $(this);
+    var parent = $this.parent(".likes");
 
-// Write your JavaScript code.
+    var counter = parent.find(".likes-count");
+
+    if (parent.find(".dislike").hasClass("active")) {
+        parent.find(".dislike").removeClass("active");
+        var dislikes = parent.find(".dislike").find(".dislikes-count");
+        dislikes.html(parseInt(dislikes.html()) - 1);
+    }
+   
+    if($this.hasClass("active")) {
+        $this.removeClass("active");
+        counter.html(parseInt(counter.html()) - 1);
+    }
+    else {
+        $this.addClass("active");
+        counter.html(parseInt(counter.html()) + 1);
+    }
+});
+
+$(".dislike").on("click", function() {
+    event.preventDefault();
+    var $this = $(this);
+    var parent = $this.parent(".likes");
+
+    if (parent.find(".like").hasClass("active")) {
+        parent.find(".like").removeClass("active");
+        var likes = parent.find(".likes-count");
+        likes.html(parseInt(likes.html()) - 1);
+    }
+
+    var counter = parent.find(".dislikes-count");
+    if($this.hasClass("active")) {
+        $this.removeClass("active");
+        counter.html(parseInt(counter.html()) - 1);
+    }
+    else {
+        $this.addClass("active");
+        counter.html(parseInt(counter.html()) + 1);
+    }    
+});
