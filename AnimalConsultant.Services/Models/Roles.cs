441e77368e62459c8bd4ef94c41f7f025bb1502f
Iryna Vasyliv <iryna.vasyliv@empeek.io>
2019-06-04 10:25:03 +0300

    questions view

---
+++ AnimalConsultant.Services/Models/Roles.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AnimalConsultant.Services.Models
+{
+    public enum Roles
+    {
+        User,
+        Vet
+    }
+}
